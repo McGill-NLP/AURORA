8902557e37bd2c22646271c073cbe96c6c900acc
Luis Lara <luis.lara@mila.quebec>
2024-07-09 08:51:01 -0400

    app.py updated

--- app.py
+++ app.py
@@ -1,34 +1,13 @@
 from __future__ import annotations
 import math
-import random
 import gradio as gr
 import torch
 from PIL import Image, ImageOps
 from diffusers import StableDiffusionInstructPix2PixPipeline
 
-example_instructions = [
-    "move the lemon to the right of the table"
-]
-
 def main():
     pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained("McGill-NLP/AURORA", safety_checker=None).to("cuda")
     example_image = Image.open("example.jpg").convert("RGB")
-
-    def load_example(
-        steps: int,
-        seed: int,
-        text_cfg_scale: float,
-        image_cfg_scale: float,
-    ):
-        example_instruction = random.choice(example_instructions)
-        return [example_image, example_instruction] + generate(
-            example_image,
-            example_instruction,
-            steps,
-            seed,
-            text_cfg_scale,
-            image_cfg_scale,
-        )
 
     def generate(
         input_image: Image.Image,
@@ -57,28 +36,26 @@ def main():
         return [seed, text_cfg_scale, image_cfg_scale, edited_image]
 
     def reset():
-        return [50, 42, 7.5, 1.5, None]
+        return ["", 50, 42, 7.5, 1.5, None, None]
 
     with gr.Blocks() as demo:
         gr.HTML("""<h1 style="font-weight: 900; margin-bottom: 10px;">
             AURORA: Learning Action and Reasoning-Centric Image Editing from Videos and Simulations
         </h1>
         <p>
-            AURORA (Action Reasoning Object Attribute) enables training an instruction-guided image editing model that can perform action and reasoning-centric edits, in addition to "simpler" established object, attribute or global edits. <b> To illustrate this, please click "Load example" </b>.
+            AURORA (Action Reasoning Object Attribute) enables training an instruction-guided image editing model that can perform action and reasoning-centric edits, in addition to "simpler" established object, attribute or global edits.
         </p>""")
         
         with gr.Row():
             with gr.Column(scale=3):
-                instruction = gr.Textbox(lines=1, label="Edit instruction", interactive=True)
+                instruction = gr.Textbox(value="move the lemon to the right of the table", lines=1, label="Edit instruction", interactive=True)
             with gr.Column(scale=1, min_width=100):
                 generate_button = gr.Button("Generate", variant="primary")
             with gr.Column(scale=1, min_width=100):
                 reset_button = gr.Button("Reset", variant="stop")
-            with gr.Column(scale=1, min_width=100):
-                load_button = gr.Button("Load example")
 
         with gr.Row():
-            input_image = gr.Image(label="Input image", type="pil", interactive=True)
+            input_image = gr.Image(value=example_image, label="Input image", type="pil", interactive=True)
             edited_image = gr.Image(label=f"Edited image", type="pil", interactive=False)
 
         with gr.Row():
@@ -86,17 +63,7 @@ def main():
             seed = gr.Number(value=42, precision=0, label="Seed", interactive=True)
             text_cfg_scale = gr.Number(value=7.5, label=f"Text CFG", interactive=True)
             image_cfg_scale = gr.Number(value=1.5, label=f"Image CFG", interactive=True)
-
-        load_button.click(
-            fn=load_example,
-            inputs=[
-                steps,
-                seed,
-                text_cfg_scale,
-                image_cfg_scale,
-            ],
-            outputs=[input_image, instruction, seed, text_cfg_scale, image_cfg_scale, edited_image],
-        )
+    
         generate_button.click(
             fn=generate,
             inputs=[
@@ -112,7 +79,7 @@ def main():
         reset_button.click(
             fn=reset,
             inputs=[],
-            outputs=[steps, seed, text_cfg_scale, image_cfg_scale, edited_image],
+            outputs=[instruction, steps, seed, text_cfg_scale, image_cfg_scale, edited_image, input_image],
         )
 
     demo.queue()
